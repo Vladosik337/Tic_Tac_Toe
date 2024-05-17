14948d2dc26bd2637ea63b903ca9e88d02d81e2a
Vladosik337 <vlad.ostapenko.337@gmail.com>
2024-05-18 00:27:05 +0300

    Update Program.cs
    Add Program entry point to start the game

--- Tic_Tac_Toe/Program.cs
+++ Tic_Tac_Toe/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using TicTacToe.Controllers;
+
+namespace TicTacToe
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            GameController gameController = new GameController();
+            gameController.StartGame();
+        }
+    }
+}
